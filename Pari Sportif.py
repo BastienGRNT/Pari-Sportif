8cc5e5740380b1d4f48eeb1913bcea79121aed2d
BastienGRNT <bastien.grenot@gmail.com>
2024-11-18 15:39:13 +0100

    Update Pari Sportif.py

--- Pari Sportif.py
+++ Pari Sportif.py
@@ -3,11 +3,9 @@
 
 cote_Equipe_A = float(input("Veuillez renseigner la cote de l'équipe A : "))
 cote_Equipe_B = float(input("Veuillez renseigner la cote de l'équipe B : "))
-#Instanciation des cotes pour déterminer ensuite les sommes à miser.
 
 a = range(1, 100)
 res=[0,0,0,0,0,0,0]
-#Création d'une liste vierge où seront stockées par la suite les (mises, gains, etc...).
 
 for mise_Equipe_A in a:
     for mise_Equipe_B in a:
@@ -20,12 +18,11 @@ for mise_Equipe_A in a:
             prov=[gain_Equipe_A, mise_Total, gain_Equipe_B, mise_Equipe_A, mise_Equipe_B, benefice_Equipe_A, benefice_Equipe_B]
             if (prov[5]>res[5] and prov[5]>res[6] and prov[6]>res[5] and prov[6]>res[6]):   
                 res=prov
-            #Calcul des sommes à miser à la fois sur A et sur B pour gagner dans tous les cas sauf match nul.
 
 
 print("")
-print(f"BENEF SI A GAGNANT : ", res[5])
-print(f"BENEF SI B GAGNANT : ", res[6])
+print(f"BENEFICE SI A GAGNANT : ", res[5])
+print(f"BENEFICE SI B GAGNANT : ", res[6])
 print("")
 print(f"Mise Totale : ", res[1])
 print("")
@@ -34,4 +31,3 @@ print(f"GAIN-A : ", res[0])
 print("")
 print(f"Mise-B : ", res[4])
 print(f"GAIN-B : ", res[2])
-#Affichage des sommes à miser et des gains !
